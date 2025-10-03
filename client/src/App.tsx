778845e3f955fa8efe819145eefcfc125b3753e6
[200~batanai777gw~ <batanai.gw@gmail.com>
2025-10-03 06:18:00 +0200

    folder-structure

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-5xl font-extrabold text-white drop-shadow-lg tracking-wide">
+        🚀 Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
